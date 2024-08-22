ef01094c1622a71697b93f20c7d416b8a74d0aa5
c4zombieslayer <c4zombieslayer@gmail.com>
2024-08-22 04:43:13 -0700

    js added events

--- src/system/game.js
+++ src/system/game.js
@@ -3,27 +3,19 @@ import { keyDownEvent, keyUpEvent, mouseDownEvent, mouseUpEvent, mouseMoveEvent 
 
 export  class Game {
     constructor() {
+        this.run_loop =     false;
+        this.last_time =    0;
 
-        this.run_loop = false;
         this.canvas =   document.getElementById("game-canvas");;
         this.ctx =      this.canvas.getContext("2d");
         
-        this.handleMouseDown =  ()=>mouseDownEvent(this.mouse_pos);
-        this.handleMouseUp =    ()=>mouseUpEvent(this.mouse_pos);
-        this.handleMouseMove =  ()=>mouseMoveEvent(this.mouse_pos);
+        this.handleMouseDown =  () => mouseDownEvent(this.mouse_pos);
+        this.handleMouseUp =    () => mouseUpEvent(this.mouse_pos);
+        this.handleMouseMove =  () => mouseMoveEvent(this.mouse_pos);
         this.mouse_pos =        {x: 0, y: 0};
 
         this.environment =  [];
         this.gui =          [];
-
-    }
-
-
-    gameLoop = (t) => {
-        console.log(t)
-        
-        // Request next frame.
-        if(this.run_loop) requestAnimationFrame(this.gameLoop);
     }
 
 
@@ -55,5 +47,57 @@ export  class Game {
         // Clear game assets.
         this.environment =  [];
         this.gui =          [];
+    }
+
+
+    getDeltaTime(time) {
+        // Convert to seconds.
+        time *= 0.001;
+
+
+        // Calculate delta time.
+        let delta_time = time - (this.last_time || time);
+        this.last_time = time;
+
+
+        // Return delta time.
+        return delta_time;
+    }
+
+
+    tick(delta_time) {
+        // Tick game.
+        for(let i = 0; i < this.environment.length; ++i) {
+            this.environment[i].tick(delta_time, this.mouse_pos);
+        }
+        for(let i = 0; i < this.gui.length; ++i) {
+            this.gui[i].tick(delta_time, this.mouse_pos);
+        }
+    }
+
+
+    draw(delta_time) {
+        // Draw game.
+        for(let i = 0; i < this.environment.length; ++i) {
+            this.environment[i].draw(this.ctx);//TODO imagelib.
+        }
+        for(let i = 0; i < this.gui.length; ++i) {
+            this.gui[i].draw(this.ctx);
+        }
+    }
+
+
+    gameLoop = (time) => {
+        // Get delta time.
+        let delta_time = this.getDeltaTime(time);
+        
+
+        // Tick and draw game.
+        this.tick(delta_time);
+        this.draw(delta_time);
+
+
+        // Request next frame.
+        if(this.run_loop) requestAnimationFrame(this.gameLoop);
     }
 }
